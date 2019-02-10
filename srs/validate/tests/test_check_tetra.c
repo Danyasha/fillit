389a191a44a15b8ff022064edcc8d2e03e4bd212
Bessie Torp <btorp@e2r8p5.21-school.ru>
2019-02-10 19:09:54 +0300

    обновил тест чек тетра

--- srs/validate/tests/test_check_tetra.c
+++ srs/validate/tests/test_check_tetra.c
@@ -6,7 +6,7 @@
 /*   By: btorp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/10 16:47:15 by btorp             #+#    #+#             */
-/*   Updated: 2019/02/10 17:10:13 by btorp            ###   ########.fr       */
+/*   Updated: 2019/02/10 18:58:59 by btorp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ int		main(void)
 	char	*line;
 	char	**lines;
 	int		i;
+	int		f;
 
+	f = 0;
 	fd = open("test.fillit", O_RDONLY);
 	lines = (char**)malloc(sizeof(char*)*5);
 	lines[4] = NULL;
@@ -30,18 +32,28 @@ int		main(void)
 		{
 			if(ft_check_tetra(lines) == 0)
 			{
-				for (int g = 0; g < 4; g++)
-				{
-					printf("%s\n", lines[g]);
-				}
+				f++;
 				printf("invalid\n");
+				for (int g = 0; g < 4; g++)
+					printf("%s\n", lines[g]);
 			}
+			else
+			{
+				printf("valid\n");
+				for (int g = 0; g < 4; g++)
+					printf("%s\n", lines[g]);
+			}
+			
 			i = 0;
 			continue ;
 		}
 		lines[i] = line;
 		i++;
 	}
+	if (f)
+		printf("Count of non-valid tetras = %i\n", f);
+	else 
+		printf("All tetras are valid\n");
 	close(fd);
 	return (0);
 }
