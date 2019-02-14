9513c090134ade0e41955d15f62314df4ef547e2
Bessie Torp <btorp@e1r11p6.21-school.ru>
2019-02-14 22:10:47 +0300

    написан тест для делета

--- srs/backtraking/backtraking.h
+++ srs/backtraking/backtraking.h
@@ -6,7 +6,7 @@
 /*   By: btorp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 15:21:42 by btorp             #+#    #+#             */
-/*   Updated: 2019/02/14 17:05:35 by btorp            ###   ########.fr       */
+/*   Updated: 2019/02/14 22:04:19 by btorp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,5 @@ int		backtraking_main(t_dlst **head);
 t_map	*find_sol(t_map map, t_dlst temp, int x, int y);
 t_map	*map_gen(t_dlst *t, int size);
 int		tetra_place(t_map *map, t_dlst *tet, int x, int y);
+void	tetra_del(t_map *map, t_dlst *tet);
 #endif
